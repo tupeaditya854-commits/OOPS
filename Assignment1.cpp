7ac96c991ec1856bbb06fdc583b57432acfc6a8b
tupeaditya854-commits <tupeaditya854@gmail.com>
2026-08-26 23:27:16 +0530

    Add files via upload

---
+++ Assignment1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Book {
+public:
+    string title;
+    string author;
+    float price;
+    int bookid;
+
+    void display() {
+        cout << "Title: " << title << endl;
+        cout << "Author: " << author << endl;
+        cout << "Price: " << price << endl;
+        cout << "Book ID: " << bookid << endl;
+    }
+};
+
+int main() {
+    Book book1, book2;
+
+    cout << "Enter details for Book 1:\n";
+    cout << "Title: ";
+    cin >> book1.title;
+    cout << "Author: ";
+    cin >> book1.author;
+    cout << "Price: ";
+    cin >> book1.price;
+    cout << "Book ID: ";
+    cin >> book1.bookid;
+
+    cout << "\nEnter details for Book 2:\n";
+    cout << "Title: ";
+    cin >> book2.title;
+    cout << "Author: ";
+    cin >> book2.author;
+    cout << "Price: ";
+    cin >> book2.price;
+    cout << "Book ID: ";
+    cin >> book2.bookid;
+
+    cout << "\nBook 1 details:\n";
+    book1.display();
+    cout << "\nBook 2 details:\n";
+    book2.display();
+
+    return 0;
+}
